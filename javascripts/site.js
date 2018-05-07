4d650d4c64fee4659d6f35969a70f5ea8324358e
maisonn <37782167+maisonn@users.noreply.github.com>
2018-05-08 02:23:34 +0300

    Update
    1

---
+++ javascripts/site.js
@@ -0,0 +1,8 @@
+// This is where it all goes :)
+function openNav() {
+    document.getElementById("myBottomnav").style.height = "15%";
+}
+
+function closeNav() {
+    document.getElementById("myBottomnav").style.height = "0";
+}
